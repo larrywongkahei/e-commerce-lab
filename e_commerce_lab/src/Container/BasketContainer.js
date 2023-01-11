76b94b3c76bdca5b9cd60e2c1707bb532b76955b
Maryhei <makemak123@gmail.com>
2023-01-11 16:27:53 +0000

    Added Total

--- e_commerce_lab/src/Container/BasketContainer.js
+++ e_commerce_lab/src/Container/BasketContainer.js
@@ -1,14 +1,23 @@
 import React from 'react';
 
 const BasketContainer = ({basket}) => {
+const arrayOfPrice = []
 
 const basketItems = basket.map((item) => {
+    arrayOfPrice.push(parseInt(item.price))
     return <li>{item.name}{item.price}</li>
 })
+const initialValue = 0;
+const sumWithInitial = arrayOfPrice.reduce(
+  (accumulator, currentValue) => accumulator + currentValue,
+  initialValue
+);
+
 
     return(
         <ul>
             {basketItems}
+            {sumWithInitial}
         </ul>
     )
 }
