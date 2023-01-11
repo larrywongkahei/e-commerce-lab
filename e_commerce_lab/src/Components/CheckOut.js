b35d4a0f1a09ccba3cb314ff3be62204d7b8e198
ljones-11 <116349877+ljones-11@users.noreply.github.com>
2023-01-11 21:55:45 +0000

    basic css for checkout

--- e_commerce_lab/src/Components/CheckOut.js
+++ e_commerce_lab/src/Components/CheckOut.js
@@ -5,23 +5,27 @@ const Checkout = () =>{
     const navigate = useNavigate()
 
     const handlePay = () =>{
-       alert("Failed to pay")
-       return navigate("/Home")
+    alert("Failed to pay")
+    return navigate("/Home")
     }
 
 
 
     return (
         <>
-        <label>Card number:</label>
-        <input type="text" placeholder="Enter your card number"></input>
-        <label>Card holder name:</label>
-        <input type="text" placeholder="Enter your name"></input>
-        <label>Date of expiry:</label>
-        <input type="text" maxLength="2"/><input type="text" maxLength="2"></input>
-        <h1>Total:</h1>
-        <input type="checkbox"></input>
-        <button onClick={handlePay}>Checkout</button>
+        <div className='checkout-form'>
+            <label>Card number:</label>
+            <input type="text" placeholder="Enter your card number"></input>
+            <label>Card holder name:</label>
+            <input type="text" placeholder="Enter your name"></input>
+            <label>Date of expiry:</label>
+            <input type="text" maxLength="2"/><input type="text" maxLength="2"></input>
+        </div>
+            <h1 className="h1-total">Total:</h1>
+            <div className="checkout-total-div">
+            <input type="checkbox"></input>
+            <button className="button-checkout" onClick={handlePay}>Checkout</button>
+        </div>
         </>
     )
 }
