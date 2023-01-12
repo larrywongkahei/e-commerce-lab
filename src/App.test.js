0b1436c5448c652ad3f9ad6871648914ad9d362f
Maryhei <makemak123@gmail.com>
2023-01-12 17:39:39 +0000

    Fixed bug

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render } from '@testing-library/react';
+import MainContainer from "../Container/MainContainer";
 
-test('render maincontainer', () => {
-  render(<App />);
+it('render maincontainer', () => {
+  render(<MainContainer />);
 });
